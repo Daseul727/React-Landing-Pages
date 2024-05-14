632e2e469582b9d4763e26cf75128aa89e776612
김다슬 <daseul727@gmail.com>
2024-05-14 15:16:22 +0900

    basic- add footer 반응형

--- basic/components/layout/Footer.tsx
+++ basic/components/layout/Footer.tsx
@@ -5,16 +5,16 @@ const Footer = () => {
     return (
         <>
             <div className="footer">
-                <div className="footer_box">
+                <div className="footer-item">
                     <h3>주소</h3>
                     <p>주소를 입력하세요.</p>
                 </div>
-                <div className="footer_box">
+                <div className="footer-item">
                     <h3>연락처</h3>
                     <p>전화번호: 010-1234-5678</p>
                     <p>이메일: example@example.com</p>
                 </div>
-                <div className="footer_box">
+                <div className="footer-item">
                     <h3>고객센터</h3>
                     <p>고객센터 연락처: 123-456-7890</p>
                     <p>이메일: support@example.com</p>
--- basic/components/layout/Header.tsx
+++ basic/components/layout/Header.tsx
@@ -10,7 +10,6 @@ const Header = () => {
             <div className="header">
                 <img src="/img/ds_logo.jpg" alt="로고" className="logo" />
                 <div className="menu_area">
-                    <span className="menu_text">MENU</span>
                     <img src="/img/menu.png" alt="햄버거 버튼" className="menu_icon" />
                 </div>
 
